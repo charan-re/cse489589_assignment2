8c96058ec0ab6774da4516dbfc8f1335951187e1
charan-re <charanre@buffalo.edu>
2020-11-23 00:55:28 -0500

    fixed resedissue

--- grader/E2_results/E2_gbn_500.sh
+++
@@ -1,4 +0,0 @@
-./run_experiments -o E2_gbn_l_02_w500.txt -m 1000 -l 0.2 -c 0.2 -t 50 -w 500 -p ../charanre/gbn
-./run_experiments -o E2_gbn_l_05_w500.txt -m 1000 -l 0.5 -c 0.2 -t 50 -w 500 -p ../charanre/gbn
-./run_experiments -o E2_gbn_l_08_w500.txt -m 1000 -l 0.8 -c 0.2 -t 50 -w 500 -p ../charanre/gbn
-
